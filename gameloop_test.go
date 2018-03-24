d1b2a71d79504bc16d88736b5f1a35e8cc3bb3ee
Marcus Renno <me@rennomarcus.com>
2018-03-24 18:36:28 -0400

    Added gameloop test. Added setup and teardown for log fatal

---
+++ gameloop_test.go
@@ -0,0 +1,30 @@
+package macaw
+
+import (
+	"github.com/tubelz/macaw/input"
+	"github.com/tubelz/macaw/system"
+	"github.com/veandco/go-sdl2/sdl"
+	"testing"
+	"time"
+)
+
+// TestGameLoop_Run checks if our gameloop runs with a basic setup
+func TestGameLoop_Run(t *testing.T) {
+	setup(t)
+	// This is also consider setup. We are adding what it needs to be added so our game loop can be ran
+	input := &input.Manager{}
+	sceneGame := &Scene{Name: "game"}
+	sceneGame.AddRenderSystem(&system.RenderSystem{})
+	gameLoop := &GameLoop{InputManager: input}
+	gameLoop.AddScene(sceneGame)
+	// close game after 100 milliseconds
+	go func() {
+		duration := time.Millisecond
+		time.Sleep(duration * 100)
+		quitEvent := &sdl.QuitEvent{Type: sdl.QUIT, Timestamp: sdl.GetTicks()}
+		sdl.PushEvent(quitEvent)
+	}()
+	gameLoop.Run()
+
+	teardown()
+}
--- macaw_test.go
+++ macaw_test.go
@@ -3,16 +3,44 @@ package macaw
 import "testing"
 import "os/exec"
 
+var (
+	origLogFatal  = func(text ...interface{}) {}
+	origLogFatalf = func(format string, args ...interface{}) {}
+)
+
+// setup will initialize some basic configuration
+func setup(t *testing.T) {
+	// replace log.Fatal functions
+	origLogFatal = logFatal
+	logFatal = func(args ...interface{}) {
+		t.Error(args)
+		t.FailNow()
+	}
+
+	origLogFatalf = logFatalf
+	logFatalf = func(format string, args ...interface{}) {
+		t.Errorf(format, args)
+		t.FailNow()
+	}
+}
+
+// teardown frees what setup has initialized
+func teardown() {
+	// bring back original log.Fatal functions
+	logFatal = origLogFatal
+	logFatalf = origLogFatalf
+}
+
 func TestGoFmt(t *testing.T) {
 	cmd := exec.Command("gofmt", "-l", ".")
 
 	if out, err := cmd.Output(); err != nil {
 		if len(out) > 0 {
-			t.Fatalf("Exit error: %v", err)
+			t.Errorf("Exit error: %v", err)
 		}
 	} else {
 		if len(out) > 0 {
-			t.Fatal("You need to run go fmt")
+			t.Error("You need to run go fmt")
 		}
 
 	}
