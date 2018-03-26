2ca6320d3e142a548f121659f64036543d38b036
Marcus Renno <me@rennomarcus.com>
2018-03-25 21:14:48 -0400

    Updated test to use camera

--- gameloop_test.go
+++ gameloop_test.go
@@ -1,6 +1,7 @@
 package macaw
 
 import (
+	"github.com/tubelz/macaw/entity"
 	"github.com/tubelz/macaw/input"
 	"github.com/tubelz/macaw/system"
 	"github.com/veandco/go-sdl2/sdl"
@@ -14,7 +15,7 @@ func TestGameLoop_Run(t *testing.T) {
 	// This is also consider setup. We are adding what it needs to be added so our game loop can be ran
 	input := &input.Manager{}
 	sceneGame := &Scene{Name: "game"}
-	sceneGame.AddRenderSystem(&system.RenderSystem{})
+	sceneGame.AddRenderSystem(&system.RenderSystem{Camera: &entity.Entity{}})
 	gameLoop := &GameLoop{InputManager: input}
 	gameLoop.AddScene(sceneGame)
 	// close game after 100 milliseconds
