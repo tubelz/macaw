74464da7b9d291f46db42ef529c2421ce765e518
Marcus Renno <me@rennomarcus.com>
2018-08-14 20:35:39 -0400

    Closes #3. Added tests for the math package. Fixed null references in math functions.

--- math/math.go
+++ math/math.go
@@ -14,6 +14,11 @@ type FPoint struct {
 
 // SumPoint sums two Points
 func SumPoint(a *sdl.Point, b *sdl.Point) *sdl.Point {
+	if a == nil {
+		return b
+	} else if b == nil {
+		return a
+	}
 	x := a.X + b.X
 	y := a.Y + b.Y
 	return &sdl.Point{x, y}
@@ -29,25 +34,38 @@ func SumFPoint(a *FPoint, b *FPoint) *FPoint {
 	return &FPoint{a.X + b.X, a.Y + b.Y}
 }
 
-// SumPointWithFPoint sums a FPoint to a Point
+// SumPointWithFPoint sums a Point to a FPoint. It loses precision. The result is rounded.
 func SumPointWithFPoint(a *sdl.Point, b *FPoint) *sdl.Point {
-	x := float32(a.X) + b.X
-	y := float32(a.Y) + b.Y
-	return &sdl.Point{Round(x), Round(y)}
+	if b == nil {
+		return a
+	} else if a == nil {
+		return &sdl.Point{Round(b.X), Round(b.Y)}
+	}
+	rounded_b := &sdl.Point{Round(b.X), Round(b.Y)}
+	return SumPoint(a, rounded_b)
 }
 
 // ConvertPointToFPoint converts a Point to a FPoint
 func ConvertPointToFPoint(a *sdl.Point) *FPoint {
+	if a == nil {
+		return &FPoint{0, 0}
+	}
 	return &FPoint{float32(a.X), float32(a.Y)}
 }
 
 // MulFPointWithFloat multiply a FPoint with a float
 func MulFPointWithFloat(a *FPoint, b float32) *FPoint {
+	if a == nil {
+		return &FPoint{0, 0}
+	}
 	return &FPoint{a.X * b, a.Y * b}
 }
 
 // MulPointWithInt multiply a point with an int
 func MulPointWithInt(a *sdl.Point, b int32) *sdl.Point {
+	if a == nil {
+		return &sdl.Point{0, 0}
+	}
 	return &sdl.Point{a.X * b, a.Y * b}
 }
 
--- math/math_test.go
+++ math/math_test.go
@@ -1,6 +1,107 @@
 package math
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/veandco/go-sdl2/sdl"
+)
+
+func TestSumPoint(t *testing.T) {
+	cases := []struct {
+		inA  *sdl.Point
+		inB  *sdl.Point
+		want *sdl.Point
+	}{
+		{&sdl.Point{1, 2}, &sdl.Point{3, 5}, &sdl.Point{4, 7}},
+		{nil, &sdl.Point{3, 5}, &sdl.Point{3, 5}},
+		{&sdl.Point{1, 2}, nil, &sdl.Point{1, 2}},
+	}
+	for _, c := range cases {
+		got := SumPoint(c.inA, c.inB)
+		if *got != *c.want {
+			t.Errorf("SumPoint(%v, %v) == %v, want %v", c.inA, c.inB, got, c.want)
+		}
+	}
+}
+
+func TestSumFPoint(t *testing.T) {
+	cases := []struct {
+		inA  *FPoint
+		inB  *FPoint
+		want *FPoint
+	}{
+		{&FPoint{1, 2}, &FPoint{3, 5}, &FPoint{4, 7}},
+		{nil, &FPoint{3, 5}, &FPoint{3, 5}},
+		{&FPoint{1, 2}, nil, &FPoint{1, 2}},
+		{&FPoint{1.2, 2.1}, &FPoint{2.1, 2.1}, &FPoint{3.3, 4.2}},
+	}
+	for _, c := range cases {
+		got := SumFPoint(c.inA, c.inB)
+		if *got != *c.want {
+			t.Errorf("SumFPoint(%v, %v) == %v, want %v", c.inA, c.inB, got, c.want)
+		}
+	}
+}
+
+func TestSumPointWithFPoint(t *testing.T) {
+	cases := []struct {
+		inA  *sdl.Point
+		inB  *FPoint
+		want *sdl.Point
+	}{
+		{&sdl.Point{1, 2}, &FPoint{3, 5}, &sdl.Point{4, 7}},
+		{nil, &FPoint{3, 5}, &sdl.Point{3, 5}},
+		{&sdl.Point{1, 2}, nil, &sdl.Point{1, 2}},
+		{&sdl.Point{1, 2}, &FPoint{2.1, 2.1}, &sdl.Point{3, 4}},
+		{&sdl.Point{1, 2}, &FPoint{2.8, 2.8}, &sdl.Point{4, 5}},
+	}
+	for _, c := range cases {
+		got := SumPointWithFPoint(c.inA, c.inB)
+		if *got != *c.want {
+			t.Errorf("SumPointWithFPoint(%v, %v) == %v, want %v", c.inA, c.inB, got, c.want)
+		}
+	}
+}
+
+func TestConvertPointToFPoint(t *testing.T) {
+	cases := []struct {
+		in   *sdl.Point
+		want *FPoint
+	}{
+		{&sdl.Point{1, 2}, &FPoint{1, 2}},
+		{nil, &FPoint{0, 0}},
+	}
+	for _, c := range cases {
+		got := ConvertPointToFPoint(c.in)
+		if *got != *c.want {
+			t.Errorf("ConvertPointToFPoint(%v) == %v, want %v", c.in, got, c.want)
+		}
+	}
+}
+
+func TestMulFPointWithFloat(t *testing.T) {
+	cases := []struct {
+		inA  *FPoint
+		inB  float32
+		want *FPoint
+	}{
+		{&FPoint{1, 2}, 2, &FPoint{2, 4}},
+		{nil, 10, &FPoint{0, 0}},
+		{&FPoint{1.1, 2.2}, 2, &FPoint{2.2, 4.4}},
+		{&FPoint{1, 2}, 2.1, &FPoint{2.1, 4.2}},
+		{&FPoint{1.1, 2.1}, 2.1, &FPoint{1.1 * float32(2.1), 2.1 * float32(2.1)}},
+	}
+	for _, c := range cases {
+		got := MulFPointWithFloat(c.inA, c.inB)
+		if *got != *c.want {
+			t.Errorf("MulFPointWithFloat(%v, %v) == %v, want %v", c.inA, c.inB, got, c.want)
+		}
+	}
+}
+
+func TestMulPointWithInt(t *testing.T) {
+
+}
 
 func TestRound(t *testing.T) {
 	cases := []struct {
@@ -23,20 +124,32 @@ func TestRound(t *testing.T) {
 	}
 }
 
-func TestSumFPoint(t *testing.T) {
+func TestRound64(t *testing.T) {
 	cases := []struct {
-		inA  *FPoint
-		inB  *FPoint
-		want *FPoint
+		in   float64
+		want int64
 	}{
-		{&FPoint{1, 2}, &FPoint{3, 5}, &FPoint{4, 7}},
-		{nil, &FPoint{3, 5}, &FPoint{3, 5}},
-		{&FPoint{1, 2}, nil, &FPoint{1, 2}},
+		{3.14, 3},
+		{-3.14, -3},
+		{2.718, 3},
+		{3.141592653589793, 3},
+		{-3.141592653589793, -3},
+		{-2.718, -3},
+		{0, 0},
 	}
 	for _, c := range cases {
-		got := SumFPoint(c.inA, c.inB)
-		if *got != *c.want {
-			t.Errorf("SumFPoint(%v, %v) == %v, want %v", c.inA, c.inB, got, c.want)
+		got := Round64(c.in)
+		if got != c.want {
+			t.Errorf("Round64(%f) == %d, want %d", c.in, got, c.want)
 		}
+	}
+}
+
+func BenchmarkRound(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		Round(10.10)
+		Round(10.9)
+		Round(-10.10)
+		Round(-10.9)
 	}
 }
