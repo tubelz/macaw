20c5c23398a2da5d07546a7327f13c5bf862434b
Marcus Renno <me@rennomarcus.com>
2018-08-22 19:50:56 -0400

    Added more tests to the input package

--- input/input_test.go
+++ input/input_test.go
@@ -5,8 +5,8 @@ import (
 	"testing"
 )
 
-// TestManager_HandleEvents should check if we push a keyboard event it will register in our queue
-func TestManager_HandleEventsBasic(t *testing.T) {
+// TestManager_HandleEvents_basic should check if we push a keyboard event it will register in our queue
+func TestManager_HandleEvents_basic(t *testing.T) {
 	m := &Manager{}
 	buttonDownPressed := &sdl.KeyboardEvent{Type: sdl.KEYDOWN, State: sdl.PRESSED, Keysym: sdl.Keysym{Sym: sdl.K_DOWN}}
 
@@ -19,9 +19,9 @@ func TestManager_HandleEventsBasic(t *testing.T) {
 	}
 }
 
-// TestManager_HandleEventsMultiple should check if we push multiple events of the same type (keyboard)
+// TestManager_HandleEvents_multiple should check if we push multiple events of the same type (keyboard)
 // the queue of objects will be good
-func TestManager_HandleEventsMultiple(t *testing.T) {
+func TestManager_HandleEvents_multiple(t *testing.T) {
 	m := &Manager{}
 	buttonDownPressed := &sdl.KeyboardEvent{Type: sdl.KEYDOWN, State: sdl.PRESSED, Keysym: sdl.Keysym{Sym: sdl.K_DOWN}}
 	buttonLeftPressed := &sdl.KeyboardEvent{Type: sdl.KEYDOWN, State: sdl.PRESSED, Keysym: sdl.Keysym{Sym: sdl.K_LEFT}}
@@ -37,6 +37,34 @@ func TestManager_HandleEventsMultiple(t *testing.T) {
 		t.Errorf("Expecting queue event to be %v, got %v", buttonDownPressed, m.buttons[0])
 	} else if m.buttons[2] != *buttonDownReleased {
 		t.Errorf("Expecting queue event to be %v, got %v", buttonDownReleased, m.buttons[2])
+	}
+}
+
+// TestManager_HandleEvents_quit tests the quit event
+func TestManager_HandleEvents_quit(t *testing.T) {
+	cases := []struct {
+		in   sdl.Event
+		want bool
+	}{
+		// Check if we quit when we close the window or press alt+f4 | cmd+q
+		{&sdl.QuitEvent{Type: sdl.QUIT, Timestamp: 0}, false},
+		// Shouldn't have the quit event anymore
+		{&sdl.CommonEvent{}, true},
+		// Quit when we press ESC (we might remove this later)
+		{&sdl.KeyboardEvent{Type: sdl.KEYDOWN, Keysym: sdl.Keysym{Sym: sdl.K_ESCAPE}}, false},
+	}
+
+	m := &Manager{}
+	var isRunning bool
+
+	for i, c := range cases {
+		sdl.PushEvent(c.in)
+		isRunning = m.HandleEvents()
+		m.PopEvent()
+		if isRunning != c.want {
+			t.Error(c.in)
+			t.Errorf("Case %d failing. Got %v want %v", i, isRunning, c.want)
+		}
 	}
 }
 
@@ -82,6 +110,11 @@ func TestManager_PopEvent(t *testing.T) {
 
 func TestManager_Button(t *testing.T) {
 	m := &Manager{}
+	// check when button list is empty
+	m.HandleEvents()
+	if (m.Button() != sdl.KeyboardEvent{}) {
+		t.Errorf("Button() == %v; want %v", m.Button(), sdl.KeyboardEvent{})
+	}
 	// check one button first
 	buttonDownPressed := &sdl.KeyboardEvent{Type: sdl.KEYDOWN, Timestamp: 0, State: sdl.PRESSED, Keysym: sdl.Keysym{Sym: sdl.K_DOWN}}
 	sdl.PushEvent(buttonDownPressed)
